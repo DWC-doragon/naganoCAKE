12e3b7d4ec5f0dedf81e6ed0d819dd83c2aab1bd
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2023-01-18 07:32:56 +0000

    Add activestorage

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     # customer側ルーティング
 
-  scope module: 'customers' do
+  scope module: 'public' do
     root 'homes#top'
     resources :items, only: [:show, :index]
     get 'about' => 'homes#about'
