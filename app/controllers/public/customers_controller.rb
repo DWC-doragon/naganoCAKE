20a9173a085d17009cd2839dca6b3c50d8802a39
EC2 Default User <ec2-user@ip-172-31-1-184.ap-northeast-1.compute.internal>
2023-01-19 10:04:10 +0000

    [add]customer info-edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,24 @@
 class Public::CustomersController < ApplicationController
 
   def show
-@customer = current_customer
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+        @customer = current_customer
+        if @customer.update(customer_params)
+           flash[:success] = "登録情報を変更しました。"
+           redirect_to customers_my_page_path
+        else
+            render 'edit'
+        end
+    end
+
+    def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :zip_code, :address, :telephone_number)
   end
 end
