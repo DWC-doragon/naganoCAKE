47185c8d458988d2400b81a9f79ada00ce83f697
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2023-01-19 06:21:20 +0000

    topaboutpage

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,12 @@
 class Public::HomesController < ApplicationController
 
   def top
+   @product = Product.order(created_at: :desc).limit(4)
+   @genres = Genre.all
   end
 
   def about
   end
+  
+  
 end
