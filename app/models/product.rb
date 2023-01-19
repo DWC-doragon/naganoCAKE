8cbba791b10ba47668eaa6a5bbd25c74b2f4a18a
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2023-01-19 07:15:57 +0000

    新規商品登録と商品詳細の追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  belongs_to :genre
+  
+  def add_tax_price
+    (self.price * 1.08).round
+  end
+  
+   def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+   end
+
+
 end
