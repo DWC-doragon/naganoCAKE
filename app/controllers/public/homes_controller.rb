268f03fafa5b91d3ab643fd48bb2ceaf0824e6d5
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2023-01-21 07:06:06 +0000

    cartin

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-
+   @products = Product.all.order("id DESC")
    @product = Product.order(created_at: :desc).limit(4)
    @genres = Genre.all
 
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,18 +1,18 @@
 class CartItem < ApplicationRecord
-  
+
   belongs_to :product
-  belongs_to :customer 
-  
+  belongs_to :customer
+
   validates :customer_id, presence: true
 	validates :product_id, presence: true
 	validates :quantity, presence: true
-  
+
   def price  #税込み金額
-    (product.price.to_i * 1.1).floor
+    (product.price.to_i * 1.08).floor
   end
 
-  def sub_price　
-    self.price * self.quantity
+  def sub_price
+   self.price*quantity #(product.price.to_i * 1.1).floor * quantity
   end
 
   def billing_amount #請求金額
