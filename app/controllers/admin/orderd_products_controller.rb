1607161af9a141bde9782319b0b1441771202866
EC2 Default User <ec2-user@ip-172-31-39-243.ap-northeast-1.compute.internal>
2023-01-24 05:44:10 +0000

    注文詳細完成！

--- app/controllers/admin/orderd_products_controller.rb
+++ app/controllers/admin/orderd_products_controller.rb
@@ -3,13 +3,22 @@ class Admin::OrderdProductsController < ApplicationController
   def update
     @orderd_product = OrderdProduct.find(params[:id])
     @order = @orderd_product.order
-    if @orderd_product.update(orderd_product_params)
-      flash[:notice] = "successfully"
-      redirect_to admin_order_path(@order)
-    else
-      admin_order_path(@order)
+    @orderd_product.update(orderd_product_params)
+    @orderd_products = @order.orderd_products.all
+    if @orderd_product.order_details == "producing"
+       @order.status = "producing"
+       @order.save
     end
-
+    is_updated = true
+    if @orderd_product.update(orderd_product_params)
+      @orderd_products.each do |orderd_product| 
+        if orderd_product.order_details != "producted" 
+          is_updated = false 
+        end
+      end
+      @order.update(status: 3) if is_updated
+    end
+    redirect_to admin_order_path(@order)
   end
 
   private
@@ -18,3 +27,4 @@ class Admin::OrderdProductsController < ApplicationController
     params.require(:orderd_product).permit(:order_details)
   end
 end
+
